d3e473d6305daf8399fedb76cc21d79d7bf69556
Rupam058 <rupam.irp@gmail.com>
2024-03-12 11:45:29 +0530

    intial commit

---
+++ components/index.ts
@@ -0,0 +1,25 @@
+
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilters from "./CustomFilters";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetail from "./CarDetail";
+import ShowMore from "./ShowMore";
+
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomFilters,
+    SearchManufacturer,
+    CarCard,
+    CarDetail,
+    ShowMore,
+}
