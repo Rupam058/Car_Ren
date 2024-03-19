c29918f7f9640e4c7082667aed7555b506b2beb5
Rupam058 <rupam.irp@gmail.com>
2024-03-20 02:56:35 +0530

    made sticky bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,31 +1,52 @@
-import React from "react";
+"use client";
 
 import Link from "next/link";
 import Image from "next/image";
 
 import CustomButton from "./CustomButton";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
-  return (
-    <header className="w-full absolute z-10">
-      <nav className="max-w-[1440px] mx-auto flex justify-between item-center sm:px-16 px-6 py-4">
-        <Link href="/" className="flex justify-center item-center">
-          <Image
-            src="/logo.svg"
-            alt="Car Hub Logo"
-            width={118}
-            height={32}
-            className="object-contain"
-          />
-        </Link>
+  const [header, setHeader] = useState(false);
 
-        <CustomButton
-          title="Sign In"
-          btnType="button"
-          containerStyles="text-primary-blue rounded-full bg-white min-w-[130]"
-        />
-      </nav>
-    </header>
+  const scrollHeader = () => {
+    if (window.scrollY >= 20) {
+      setHeader(true);
+    } else {
+      setHeader(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHeader);
+
+    return () => {
+      window.addEventListener("scroll", scrollHeader);
+    };
+  }, []);
+
+  return (
+    <div>
+      <header className={header ? "w-full bg-primary-blue fixed z-50 py-[2px] rounded-b-lg" : "bg-transparent"}>
+        <nav className="max-w-[1440px] mx-auto flex justify-between item-center sm:px-16 px-6 py-[2px]">
+          <Link href="/" className="flex justify-center item-center">
+            <Image
+              src={header ? "/Untitled.svg" : "/logo.svg"}
+              alt="Car Hub Logo"
+              width={118}
+              height={32}
+              className="object-contain"
+            />
+          </Link>
+
+          <CustomButton
+            title="Sign In"
+            btnType="button"
+            containerStyles={header ? "text-white text-xl font-bold rounded-full bg-primary-blue min-w-[130] hover:bg-white hover:text-primary-blue":"text-primary-blue text-xl font-bold rounded-full bg-white min-w-[130] hover:bg-primary-blue hover:text-white"}
+          />
+        </nav>
+      </header>
+    </div>
   );
 };
 
